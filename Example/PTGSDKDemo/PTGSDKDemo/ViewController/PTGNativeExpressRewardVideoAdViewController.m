35887ad4cdb77fd7ee85dc88d3dcf4a9f181be81
sochi <yngjiutao@gmail.com>
2025-06-12 11:10:27 +0800

    更新demo

--- Example/PTGSDKDemo/PTGSDKDemo/ViewController/PTGNativeExpressRewardVideoAdViewController.m
+++ Example/PTGSDKDemo/PTGSDKDemo/ViewController/PTGNativeExpressRewardVideoAdViewController.m
@@ -13,7 +13,7 @@
 
 @property(nonatomic,strong)PTGNativeExpressRewardVideoAd *rewardVideoAd;
 @property(nonatomic,strong)UIButton *loadButton;
-
+@property(nonatomic,strong)UIButton *showButton;
 @end
 
 @implementation PTGNativeExpressRewardVideoAdViewController
@@ -26,16 +26,30 @@
 
 - (void)addChildViewsAndLayout {
     [self.view addSubview:self.loadButton];
-  
+    [self.view addSubview:self.showButton];
     [self.loadButton mas_makeConstraints:^(MASConstraintMaker *make) {
         make.size.mas_equalTo(CGSizeMake(100, 40));
         make.centerX.equalTo(self.view);
         make.bottom.equalTo(self.view).offset(-40);
     }];
+    
+    [self.showButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.size.mas_equalTo(CGSizeMake(100, 40));
+        make.centerX.equalTo(self.view);
+        make.bottom.equalTo(self.loadButton.mas_top).offset(-10);
+    }];
 }
 #pragma mark - action -
 - (void)buttonClicked:(UIButton *)sender {
     [self.rewardVideoAd loadAd];
+}
+- (void)showAction:(UIButton *)sender {
+    if (self.rewardVideoAd.isReady) {
+        [self.rewardVideoAd showAdFromRootViewController:self];
+    } else {
+        NSLog(@"广告过期");
+    }
+    
 }
 
 #pragma makr - PTGRewardVideoDelegate -
@@ -43,7 +57,6 @@
 /// 在此方法中调用showAdFromRootViewController 展示激励广告
 - (void)ptg_rewardVideoAdDidLoad:(PTGNativeExpressRewardVideoAd *)rewardVideoAd {
     NSLog(@"激励广告加载成功%@",rewardVideoAd);
-    [rewardVideoAd showAdFromRootViewController:self];
 }
 
 /// 激励广告失败 加载失败 播放失败 渲染失败
@@ -59,6 +72,11 @@
 /// 激励广告曝光
 - (void)ptg_rewardVideoAdDidExposed:(PTGNativeExpressRewardVideoAd *)rewardVideoAd {
     NSLog(@"激励广告曝光%@",rewardVideoAd);
+}
+
+- (void)ptg_rewardVideoAdExposedFail:(PTGNativeExpressRewardVideoAd *)rewardVideoAd error:(NSError *)error {
+    NSLog(@"激励广告曝光失败 error = %@",error);
+
 }
 
 /// 激励广告关闭
@@ -83,7 +101,11 @@
 #pragma mark - get -
 - (PTGNativeExpressRewardVideoAd *)rewardVideoAd {
     if (!_rewardVideoAd) {
-        _rewardVideoAd = [[PTGNativeExpressRewardVideoAd alloc] initWithPlacementId:@"900000400"];
+        PTGRewardedVideoModel *model = [[PTGRewardedVideoModel alloc] init];
+        model.userId = @"user id";
+        model.rewardName = @"奖励名称";
+        model.rewardAmount = 400;
+        _rewardVideoAd = [[PTGNativeExpressRewardVideoAd alloc] initWithPlacementId:@"900000400" rewardedVideoModel:model];
         _rewardVideoAd.delegate = self;
     }
     return _rewardVideoAd;
@@ -98,6 +120,18 @@
         [_loadButton addTarget:self action:@selector(buttonClicked:) forControlEvents:UIControlEventTouchUpInside];
     }
     return _loadButton;
+}
+
+
+- (UIButton *)showButton {
+    if (!_showButton) {
+        _showButton= [UIButton buttonWithType:UIButtonTypeCustom];
+        [_showButton setTitle:@"展示广告" forState:UIControlStateNormal];
+        [_showButton setBackgroundColor:UIColor.lightGrayColor];
+        _showButton.titleLabel.font = [UIFont systemFontOfSize:15];
+        [_showButton addTarget:self action:@selector(showAction:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _showButton;
 }
 
 @end
